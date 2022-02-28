d2581247a0284e5cde05008f5558712501d2ae8b
MariamElZaatari <mariamalzaatari@gmail.com>
2022-02-28 16:35:39 +0200

    Grid Responsive Testing, NavBar Added

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ import LandPage from "./pages/LandPage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <Router>
+
+      <NavBar />
+      
       <Routes>
         <Route path='/landPage' element={<LandPage />} />
         <Route path='/login' element={<Login />} />
