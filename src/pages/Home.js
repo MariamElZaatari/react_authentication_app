3767f7056f4e08f9bfa6621da01c288250e85f3e
MariamElZaatari <mariamalzaatari@gmail.com>
2022-02-28 19:39:15 +0200

    User Context Tested, Login Handler Added Using Auth Service

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { UserContext } from '../context/UserContext'
+
 
 export default function Home() {
 
+  const { user, setUser } = useContext(UserContext);
+
   return (
     <div>
-      Hello 
+      Hello {user.first_name}
   </div>
   )
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from 'react'
+import React, { Fragment, useContext, useState } from 'react'
 import { UserContext } from '../context/UserContext'
 import AuthService from '../services/AuthService';
 
@@ -6,13 +6,32 @@ export default function Login() {
 
   const { user, setUser } = useContext(UserContext);
 
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
   const loginHandler = () => {
-    // AuthService.Login()
+    AuthService.Login(email, password)
+      .then(({ data }) => {
+        console.log('response', data)
+        setUser({
+          id: data.user.id,
+          email: data.user.email,
+          first_name: data.user.first_name,
+          last_name: data.user.last_name,
+          age: data.user.age,
+          gender: data.user.gender,
+          created_at: data.user.created_at,
+          access_token: data.access_token,
+          token_type: data.token_type,
+          expires_in: data.expires_in
+        })
+      })
   }
 
   return (
     <Fragment>
+      <input onChange={(e) => setEmail(e.target.value)} type="text" />
+      <input onChange={(e) => setPassword(e.target.value)} type="password" />
       <button onClick={loginHandler}>Login</button>
     </Fragment>
 
