5e421c8a684c6158223c35a51a3ad0a253e7bb6f
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-01 00:35:25 +0200

    Contact Service Added

---
+++ src/services/ContactService.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+const PostMessage = async (email, subject, text) => {
+    return await axios.post("http://127.0.0.1:8000/api/auth/login", 
+    {
+        email: email,
+        subject: subject,
+        text: text
+    })
+}
+
+export default { PostMessage };
