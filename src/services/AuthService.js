ff78fb93892d6bed93a9147fcf8f2f9a59cfcf3c
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-01 02:06:50 +0200

    Auth Service Added Register

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -8,4 +8,18 @@ const Login = async (email, password) => {
     })
 }
 
-export default {Login};
+const Register = async (email, password, password_confirmation, first_name, last_name, gender, age) => {
+    return await axios.post("http://127.0.0.1:8000/api/auth/register", 
+    {
+        email: email,
+        password: password,
+        password_confirmation: password_confirmation,
+        first_name: first_name,
+        last_name: last_name,
+        gender: gender,
+        age: age,
+    })
+}
+
+
+export default {Login, Register};
