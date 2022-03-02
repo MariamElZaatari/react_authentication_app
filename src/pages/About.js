4d00a1f2919f4d1f28ab7f3c6a6128412bd67815
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-02 02:19:27 +0200

    About Page Restructured and Styled

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,33 +2,41 @@ import React from 'react'
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 export default function About() {
   return (
-    <div>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '84vh' }}
+    >
 
-      <Container maxWidth="lg" className="section">
-        <Grid container rowSpacing={2} spacing={2} justifyContent="center">
+      <Grid item xs={3} className="clr_white" width="100%" backgroundColor="#5ACE7F">
 
-          <Grid item xs={12} sm={6} md={6}>
-            <Typography gutterBottom variant="h4" component="div" pt={2} pb={2} align="center" fontWeight={300} className="clr_brown_text">
-              About Us
-            </Typography>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={6}>
-            <Typography gutterBottom variant="h5" component="div" pt={2} pb={2} align="center" fontWeight={500} >
-              The Butler & Chef Team is dedicated to transforming your day-to-day life into an easier and well adjusted lifestyle.
-            </Typography>
-          </Grid>
-
-          <Grid item xs={12} sm={12} md={12}>
-            <Typography gutterBottom variant="h6" component="div" pt={2} pb={2} align="center" fontWeight={400} >
-              Become your own butler by shoppig for ingredients and deciding on a recipe. Moreover, we will help you become your own chef by providing endless recipes ranging from chefs to your friends, and even creating your own recipes and sharing them with your followers.
-            </Typography>
-          </Grid>
-
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h4" component="div" pt={2} pb={2} align="center" fontWeight={300} className="clr_white_text title">
+            About Us
+          </Typography>
         </Grid>
-      </Container></div>
+
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h5" component="div" pt={2} pb={2} align="center" fontWeight={500} className="clr_white_text subtitle">
+            In the midst of the chaos we are living in, we had to think about how we can offer you order.
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+              <Typography gutterBottom variant="h6" component="div" pt={2} pb={2} align="center" fontWeight={400} className="clr_white_text">
+                Become your own butler by shoppig for your kitchen and deciding on a recipe swiftly. <br/>Moreover, we will help you become your own chef by creating and posting your own recipes to share.
+              </Typography>
+        </Grid>
+
+      </Grid>
+    </Grid>
   )
 }
